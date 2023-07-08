da7e120740210025da2a0af11becc62adcbd8997
FabioSinobre <engenharia.computacao4.0@gmail.com>
2023-07-08 19:44:58 -0300

    create project Secret Word

---
+++ src/Data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ['motor', 'porta', 'capo', 'pneu', 'vidro', 'banco', 'volante'],
+    fruta: ['banana', 'pera', 'maça', 'mamao', 'laranja', 'tangerina', 'morango', 'melancia'],
+    corpo: ['braço', 'pernas', 'cerebro', 'pescoço', 'olhos', 'lingua', 'boca', 'pelo', 'dedos'],
+    computador: ['mouse', 'teclado', 'monitor', 'gabinete', 'fonte', 'fone', 'memoria', 'processador'],
+    programação: ['linguagem', 'framework', 'javascript', 'java', 'sintaxe', 'semantica'],
+    comida: ['arroz', 'feijao', 'carne', 'ovos', 'frutas', 'peixe', 'queijo', 'legumes', 'bolo']
+}
