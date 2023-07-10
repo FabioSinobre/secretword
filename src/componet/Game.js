3f01775c4596d9db799200afdb5284b965d4fbc1
FabioSinobre <engenharia.computacao4.0@gmail.com>
2023-07-10 20:26:51 -0300

    main page css completion

--- src/componet/Game.js
+++ src/componet/Game.js
@@ -2,9 +2,31 @@ import './Game.css'
 
 const Game = ({verifyLetter}) => {
     return(
-       <div>
-         <h3>Game</h3>
-         <button onClick={verifyLetter}>Finalizar jogo</button>
+       <div className='game'>
+         <p className='points'>
+            <span>Pontuação: 0000</span>
+         </p>
+         <h1>Adivinhe a Palavra: </h1>
+         <h3 className='tip'>Dica da palavra: <span>Dica ...</span></h3>
+         <div className='wordConteiner'>
+            <span className='letter'> A</span>
+            <span className='letter'> a</span>
+            <span className='blankSquare'></span>
+         </div>
+         <div className='letterConteiner'>
+            <p>Escolha uma letra para continuar?</p>
+            <form>
+                <input type="text" name='letter' maxLength='1' required />
+                <button>Jogar</button>
+            </form>
+        </div>
+            <div className='wrongLettersConteiner'>
+                <p>Letras já utilizadas:</p>
+                <span>a, </span>
+                <span>b, </span>
+            </div>
+
+         
        </div>
     )
 }
