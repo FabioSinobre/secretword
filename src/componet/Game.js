20472cae33e1a264ddac4105ba2c4805a6e39c4b
FabioSinobre <engenharia.computacao4.0@gmail.com>
2023-07-14 16:27:38 -0300

    inset dynamic letters

--- src/componet/Game.js
+++ src/componet/Game.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useRef } from 'react'
 import './Game.css'
 
 const Game = ({
@@ -11,7 +12,16 @@ const Game = ({
     guesses,
     score
 }) => {
-    const [letter, useLetter] = useState('')
+    const [letter, setLetter] = useState('')
+    const letterInputRef = useRef(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        verifyLetter(letter)
+        setLetter('')
+        letterInputRef.current.focus()
+    }
 
     return(
        <div className='game'>
@@ -34,15 +44,24 @@ const Game = ({
          
          <div className='letterConteiner'>
             <p>Escolha uma letra para continuar?</p>
-            <form>
-                <input type="text" name='letter' maxLength='1' required />
+            <form onSubmit={handleSubmit} >
+                <input 
+                    type="text" 
+                    name='letter' 
+                    maxLength='1' 
+                    required 
+                    onChange={(e) => setLetter(e.target.value)}
+                    value={letter}
+                    ref={letterInputRef}
+                />
                 <button>Jogar</button>
             </form>
         </div>
             <div className='wrongLettersConteiner'>
                 <p>Letras já utilizadas:</p>
-                <span>a, </span>
-                <span>b, </span>
+                {wrongLetters.map((letter, i) => (
+                    <span key={i} >{letter}, </span>
+                ) )}
             </div>
        </div>
     )
